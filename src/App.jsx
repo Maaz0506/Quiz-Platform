896b97c761b9a478681428a63d9ab3040ae08db8
Usaid12 <usaida863@gmail.com>
2023-09-09 15:16:39 +0500

    Login Context created

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Signup from "./components/forms/Signup";
 import Login from "./components/forms/Login";
 import Dashboard from "./components/dashboard";
+import { DataProvider } from "../context/LoginContext";
 
 const App = () => {
   return (
     <>
-      <BrowserRouter>
+    <DataProvider>     
         <Routes>
           <Route path="/" element={<Signup />} />
           <Route path="/login" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-      </BrowserRouter>
+        </Routes>   
+      </DataProvider>
     </>
   );
 };
