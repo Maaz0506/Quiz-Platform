9af3d0fda942bccb5bece9ae774ff53bb9fd409a
ateeb <ateebnaveed1996@gmail.com>
2023-09-09 13:37:09 +0500

    Modified the navbar according to the Quiz app requirements along with the logout button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,37 +1,47 @@
 import { React, useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Button from "react-bootstrap/Button"; 
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  // Function to handle logout
+  const handleLogout = () => {
+    // Perform logout logic here (e.g., clear session, remove tokens, etc.)
+    // After logout, navigate to the login or home page
+    navigate("/login"); // Replace with the actual route for your login page
+  };
+
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Brand href="#home">
+            <img
+              src="src\assets\logo.png"
+              width="35"
+              height="35"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />
+      </Navbar.Brand>
+        <Navbar.Brand href="#home">QUIZ APP</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="#link">My Scores</Nav.Link>
+            <Nav.Link href="#link">Profile</Nav.Link>
+            <Nav.Link href="#link">About</Nav.Link>
           </Nav>
+          <Button variant="outline-primary"  onClick={handleLogout}>
+            Logout
+          </Button>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
